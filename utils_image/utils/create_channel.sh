ee1f40282269014cad20d63ea1920a12fe27134e
Mihir Shah <mrshah@us.ibm.com>
2017-05-15 10:03:33 -0400

    Moving fetch block to join channel

--- utils_image/utils/create_channel.sh
+++ utils_image/utils/create_channel.sh
@@ -12,6 +12,4 @@ FABRIC_CFG_PATH=/shared/utils ./generate_channel_block.sh ${CHANNEL_NAME}
 echo "Creating channel ${CHANNEL_NAME}"
 CORE_LOGGING_LEVEL=debug PEER_CFG_PATH=/shared/utils CORE_PEER_MSPCONFIGPATH=/shared/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/ peer channel create -o ${ORDERER_URL} -c ${CHANNEL_NAME} -f ${CHANNEL_NAME}.tx 
 
-echo "Fetching channel block for ${CHANNEL_NAME}"
-CORE_LOGGING_LEVEL=debug PEER_CFG_PATH=/shared/utils CORE_PEER_MSPCONFIGPATH=/shared/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/ peer channel fetch -o ${ORDERER_URL} -c ${CHANNEL_NAME}
 set -e
