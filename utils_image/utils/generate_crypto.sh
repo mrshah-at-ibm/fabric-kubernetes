ac457db6dc9fd1d32a7fca1867d0bd8b86c9465a
Mihir Shah <mrshah@us.ibm.com>
2017-05-15 10:13:54 -0400

    Rename all _sk files to key.pem

--- utils_image/utils/generate_crypto.sh
+++ utils_image/utils/generate_crypto.sh
@@ -1,3 +1,8 @@
 echo "Generating Crypto Material"
 cryptogen generate --config=crypto-config.yaml
 cp -r crypto-config /shared/
+
+# Delete admin certs so anyone on the org can perform transaction
+find /shared/ -iname admincerts | xargs -i {} rm -rf {}/*
+
+for file in $(find /shared/ -iname *_sk); do dir=$(dirname $file); mv ${dir}/*_sk ${dir}/key.pem; done
